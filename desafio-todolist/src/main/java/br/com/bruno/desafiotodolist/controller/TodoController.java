4e78e961d72a8757c67bb548df4dfa648219eb98
Bruno <brunohenrrique.0303@gmail.com>
2024-02-14 23:15:09 -0300

    Adicionando métodos PUT e DELETE

--- desafio-todolist/src/main/java/br/com/bruno/desafiotodolist/controller/TodoController.java
+++ desafio-todolist/src/main/java/br/com/bruno/desafiotodolist/controller/TodoController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/todos")
@@ -24,10 +25,37 @@ public class TodoController {
     }
 
     @PostMapping
-    public ResponseEntity registerTodo(@RequestBody @Valid RequestTodo requestTodo){
-        Todo newTodo = new Todo(requestTodo);
+    public ResponseEntity registerTodo(@RequestBody @Valid RequestTodo data){
+        Todo newTodo = new Todo(data);
         todoRepository.save(newTodo);
         return ResponseEntity.ok(newTodo);
+    }
+
+    @PutMapping
+    public ResponseEntity updateTodo(@RequestBody @Valid RequestTodo data){
+        Optional<Todo> optionalTodo = todoRepository.findById(data.id());
+        if(optionalTodo.isPresent()){
+            Todo todo = optionalTodo.get();
+            todo.setName(data.name());
+            todo.setDescription(data.description());
+            todo.setPriority(data.priority());
+            todoRepository.save(todo);
+            return ResponseEntity.ok(todo);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteTodo(@PathVariable("id") String id){
+        Optional<Todo> optionalTodo = todoRepository.findById(id);
+        if(optionalTodo.isPresent()){
+            Todo todo = optionalTodo.get();
+            todoRepository.delete(todo);
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
     }
 
 
--- desafio-todolist/src/main/java/br/com/bruno/desafiotodolist/domain/RequestTodo.java
+++ desafio-todolist/src/main/java/br/com/bruno/desafiotodolist/domain/RequestTodo.java
@@ -2,5 +2,5 @@ package br.com.bruno.desafiotodolist.domain;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record RequestTodo(@NotBlank String name,@NotBlank String description,@NotBlank String priority) {
+public record RequestTodo(String id, @NotBlank String name,@NotBlank String description,@NotBlank String priority) {
 }
